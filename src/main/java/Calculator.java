45234817ede54191aa86db9ffb7a3f6cab3ab30c
anaplb3 <anapaulalimabs@gmail.com>
2022-03-03 10:43:58 -0300

    methods and tests sum, subtraction, multiplication and testSumAndSubtraction created

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,4 +3,21 @@ public class Calculator {
     public String helloWorld() {
         return "Hello World!";
     }
+
+    public int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int subtraction(int number1, int number2){
+        return number1 - number2;
+    }
+
+    public int multiplication(int number1, int number2){
+        return number1 * number2;
+    }
+
+    public int sumAndSubtraction(int number1, int number2){
+        return (number1 + number2) - (number1 + number1);
+
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -17,5 +17,32 @@ class CalculatorTest {
         String expected = "Hello World!";
         assertEquals(expected, result);
     }
+    @Test
+    void testSum() {
+        int expected = 1+1;
+        int result = calculator.sum(1,1);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void testSubtraction() {
+        int expected = 2-1;
+        int result = calculator.subtraction(2, 1);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testMultiplication() {
+        int expected = 2*2;
+        int result = calculator.multiplication(2, 2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testSumAndSubtraction(){
+        int expected = (1+2) - (1+1);
+        int result = calculator.sumAndSubtraction(1, 2);
+        assertEquals(expected, result);
+    }
 
 }
